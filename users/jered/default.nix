6fc3330a026241b5e9e893f61e2761eb0ef9b2c2
Jered Little <jeredlittle1996@gmail.com>
2023-10-07 17:56:31 -0600

    add git user info

--- users/jered/default.nix
+++ users/jered/default.nix
@@ -20,8 +20,8 @@
 
   programs.git = {
     enable = true;
-    userEmail = "jeredlittle1996@gmail.com"
-    userName = "Jered Little"
+    userEmail = "jeredlittle1996@gmail.com";
+    userName = "Jered Little";
     extraConfig = {
       safe.directory = [ "*" ];
       credential.helper = "! gh auth git-credential";
