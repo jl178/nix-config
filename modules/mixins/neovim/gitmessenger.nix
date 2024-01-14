3d643d0e9e9eebe2a068f44701337e66fa079610
Jered Little <jeredlittle1996@gmail.com>
2024-01-14 06:43:23 -0700

    add border to gitmessenger

--- modules/mixins/neovim/gitmessenger.nix
+++ modules/mixins/neovim/gitmessenger.nix
@@ -2,14 +2,13 @@
   programs.nixvim = {
     plugins.gitmessenger = {
       enable = true;
+      floatingWinOps = { border = "rounded"; };
     };
-    keymaps = [
-      {
-        mode = "n";
-        key = "<leader>G";
-        action = "<CMD>GitMessenger<cr>";
-        options = { desc = "Toggle Git Message"; };
-      }
-    ];
+    keymaps = [{
+      mode = "n";
+      key = "<leader>G";
+      action = "<CMD>GitMessenger<cr>";
+      options = { desc = "Toggle Git Message"; };
+    }];
   };
 }
