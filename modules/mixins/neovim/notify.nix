b3193e06debfbbcb6d18fc7bb2c4a71a1b1e7bdc
Jered Little <jeredlittle1996@gmail.com>
2024-01-14 09:51:46 -0700

    update notify

--- modules/mixins/neovim/notify.nix
+++ modules/mixins/neovim/notify.nix
@@ -1,7 +1,12 @@
 {
   programs.nixvim.plugins.notify = {
     enable = true;
-    fps = 60;
+    fps = 75;
     backgroundColour = "#000000";
+    maxWidth = 40;
+    extraOptions = {
+      render = "wrapped-compact";
+      stages = "slide";
+    };
   };
 }
