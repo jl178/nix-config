5ae369537c5b68e7ffd056dec0fda651e4761105
Jered Little <jeredlittle1996@gmail.com>
2024-01-14 06:47:57 -0700

    changed gitsigns chars

--- modules/mixins/neovim/gitsigns.nix
+++ modules/mixins/neovim/gitsigns.nix
@@ -1,12 +1,13 @@
 {
-    programs.nixvim.plugins.gitsigns = {
-        enable = true;
-        signs = {
-            add.text = "+";
-            change.text = "~";
-            delete.text = "_";
-            topdelete.text = "‾";
-            changedelete.text = "~";
-        };
+  programs.nixvim.plugins.gitsigns = {
+    enable = true;
+    signs = {
+      add.text = "│";
+      change.text = "│";
+      delete.text = "_";
+      topdelete.text = "‾";
+      changedelete.text = "~";
+      untracked.text = "┆";
     };
+  };
 }
