3ef79f171207f705106e2f5c21659cf7b3582487
Jered Little <jeredlittle1996@gmail.com>
2024-01-19 04:31:58 -0700

    changed copilot bind

--- modules/mixins/neovim/copilot.nix
+++ modules/mixins/neovim/copilot.nix
@@ -3,7 +3,7 @@
     plugins.copilot-vim.enable = true;
     extraConfigLua = ''
       vim.g.copilot_no_tab_map = true
-      vim.api.nvim_set_keymap("i", "<C-Tab>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+      vim.api.nvim_set_keymap("i", "<C-A>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
     '';
   };
 }
