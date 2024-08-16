554729f20d6b4469e55c78d1a367db8f2371f0d0
Jered Little <jered.little@zoominfo.com>
2024-08-16 07:33:37 -0600

    fix: syntax highlighting with treesitter

--- modules/mixins/neovim/treesitter.nix
+++ modules/mixins/neovim/treesitter.nix
@@ -1,10 +1,9 @@
 {
-    programs.nixvim.plugins.treesitter = {
-        enable = true;
-        folding = true;
-        indent = true;
-    };
-    programs.nixvim.plugins.treesitter-textobjects = {
-        enable = true;
-    };
+  programs.nixvim.plugins.treesitter = {
+    enable = true;
+    folding = true;
+    indent = true;
+  };
+  programs.nixvim.plugins.treesitter.settings.highlight.enable = true;
+  programs.nixvim.plugins.treesitter-textobjects = { enable = true; };
 }
